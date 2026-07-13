f6bf4508fde5bc55047739c776feb3a322cf902e
Martina Roby Culasso <138611794+martinarobyculasso@users.noreply.github.com>
2026-07-13 23:08:54 +0200

    Revert "modified both launchers to use argparse and follow a similar structure"
    This reverts commit 00c40ce22e1749c40405bc556a1a34599fbb782f.

--- CamIO_replication/camio_creator_launcher.py
+++ CamIO_replication/camio_creator_launcher.py
@@ -1,32 +1,4 @@
-# CamIO Creator launcher
-# lets users choose an asset image and a microphone input device before running the application
-# -- this means the app has to be launched again if the user wants to try out a different asset or mic
-
-import argparse
-from pathlib import Path
-
 from creator.app.app import main
 
-DEFAULT_ASSET = Path(__file__).resolve().parent / "creator" / "assets" / "body_anatomy.png"
-
-
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="CamIO Creator launcher")
-    parser.add_argument(
-        "--asset",
-        type=Path,
-        default=DEFAULT_ASSET,
-        help="Path to the asset image to annotate.",
-    )
-    parser.add_argument(
-        "--mic",
-        type=int,
-        default=None,
-        help="Index of the microphone input device to use.",
-    )
-    return parser.parse_args()
-
-
 if __name__ == "__main__":
-    args = parse_args()
-    main(args.asset, args.mic)
+    main()
